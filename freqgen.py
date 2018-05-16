1e715ea3c86d9012e963830285346e4d0e55f67b
Benjamin Lee <benjamindlee@me.com>
2018-05-16 11:08:22 -0700

    Docs for missing kmers

--- freqgen.py
+++ freqgen.py
@@ -314,12 +314,26 @@ def k_mer_frequencies(seq, k, include_missing=False):
          'Q': 0.16666666666666666,
          'T': 0.16666666666666666}
 
-        >>> k_mer_frequencies("GATGATGGC", 3)
-        {'ATG': 0.2857142857142857,
-         'GAT': 0.2857142857142857,
-         'GGC': 0.14285714285714285,
-         'TGA': 0.14285714285714285,
-         'TGG': 0.14285714285714285}
+        >>> k_mer_frequencies("GATGATGGC", 2)
+        {'AT': 0.25, 'GA': 0.25, 'GC': 0.125, 'GG': 0.125, 'TG': 0.25}
+        
+        >>> k_mer_frequencies("GATGATGGC", 2, include_missing="dna")
+        {'AA': 0,
+         'AC': 0,
+         'AG': 0,
+         'AT': 0.25,
+         'CA': 0,
+         'CC': 0,
+         'CG': 0,
+         'CT': 0,
+         'GA': 0.25,
+         'GC': 0.125,
+         'GG': 0.125,
+         'GT': 0,
+         'TA': 0,
+         'TC': 0,
+         'TG': 0.25,
+         'TT': 0}
     '''
 
     count = Counter(k_mers(seq, k))
