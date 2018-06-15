e91c964a64c25d52d1e57b073d086df8b515aa9f
Benjamin Lee <benjamindlee@me.com>
2018-06-15 15:23:41 -0700

    Numerous CLI improvements/bugfixes

--- cli.py
+++ cli.py
@@ -17,21 +17,28 @@ def freqgen():
 @click.argument('filepath', click.Path(exists=True, dir_okay=False))
 @click.option('-k', multiple=True, type=int, help="Values of k to featurize the seqs for. May be repeated.")
 @click.option("-c", "--codon-usage", is_flag=True, help="Whether to include a codon frequency featurization.")
-@click.option("-t", "--trans-table", type=int, default=11, help="The translation table to use. Defaults to 11, the standard genetic code.")
-def featurize(filepath, k, codon_usage, trans_table):
+@click.option("-o", '--output', type=click.Path(exists=False, dir_okay=False), help="The output file.")
+def featurize(filepath, k, codon_usage, output):
     # get the sequences as strs
     seqs = []
     with open(filepath, "r") as handle:
         for seq in SeqIO.parse(handle, "fasta"):
-            seqs.append(str(seq.seq))
+            seq = str(seq.seq)
+            seqs.append(seq)
 
-    # get the k-mer k_mer_frequencies
-    for _k in k:
-        print(yaml.dump({_k: k_mer_frequencies(seqs, _k, include_missing=True)}, default_flow_style=False), end="")
+    result = {_k: k_mer_frequencies(seqs, _k, include_missing=True) for _k in k}
 
     # get the codon usage frequencies
     if codon_usage:
-        print(yaml.dump(dict(codons=codon_frequencies("".join(seqs), trans_table)), default_flow_style=False), end="")
+        for seq in seqs:
+            if len(seq) % 3 != 0:
+                raise ValueError("Cannot calculate codons for sequence whose length is not divisible by 3")
+        result["codons"] = codon_frequencies("".join(seqs))
+
+    if output:
+        yaml.dump(result, open(output, "w+"), default_flow_style=False)
+        return
+    print(yaml.dump(result, default_flow_style=False))
 
 @freqgen.command(help="Generate an amino acid sequence from FASTA")
 @click.argument('filepath', click.Path(exists=True, dir_okay=False))
@@ -40,7 +47,7 @@ def featurize(filepath, k, codon_usage, trans_table):
 @click.option("-l", "--length", type=int, help="The length of the AA sequence (excluding stop codon) to generate if --mode=freq.")
 @click.option("-s", "--stop-codon", is_flag=True, default=True, help="Whether to include a stop codon. Defaults to true.")
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Whether to print final result if outputting to file. Defaults to false.")
-@click.option("-o", '--output', type=click.Path(exists=False, dir_okay=False))
+@click.option("-o", '--output', type=click.Path(exists=False, dir_okay=False), help="The output file.")
 def aa(filepath, mode, trans_table, length, stop_codon, output, verbose):
 
     # translate the DNA seq, if using exact AA seq
@@ -50,10 +57,14 @@ def aa(filepath, mode, trans_table, length, stop_codon, output, verbose):
         except Bio.Data.CodonTable.TranslationError:
             print("Sequence is not able to be translated! Is it already an amino acid sequence?")
             return
-        if "*" in aa_seq:
-            aa_seq.replace("*", "")
+        aa_seq = aa_seq.replace("*", "")
 
     elif mode == "freq":
+        # ensure we know how ling the new sequence should be
+        if not length:
+            print("Must provide lenght parameter using -l INTEGER")
+            return
+
         seqs = []
         # extract the sequences from the reference set
         with open(filepath, "r") as handle:
@@ -66,11 +77,12 @@ def aa(filepath, mode, trans_table, length, stop_codon, output, verbose):
 
         # make them into one big sequence
         seqs = "".join(seqs)
-        seqs.replace("*", "")
+        seqs = seqs.replace("*", "")
 
         # generate a new sequence of the right length
         aa_seq = amino_acid_seq(length, k_mer_frequencies(seqs, 1))
 
+    # add a stop codon, if requested
     if stop_codon:
         aa_seq += "*"
 
@@ -85,8 +97,8 @@ def aa(filepath, mode, trans_table, length, stop_codon, output, verbose):
         print(aa_seq)
 
 @freqgen.command(help="Generate a new DNA sequence with matching features")
-@click.option("-a", '--aa-seq', type=click.Path(exists=True, dir_okay=False))
-@click.option("-f", '--freqs', type=click.Path(exists=True, dir_okay=False))
+@click.option("-s", '--seq', type=click.Path(exists=True, dir_okay=False), help="The target amino acid sequence.")
+@click.option("-f", '--freqs', type=click.Path(exists=True, dir_okay=False), help="The target frequencies.")
 @click.option("-v", "--verbose", is_flag=True, default=False, help="Whether to show optimization progress. Defaults to false.")
 @click.option("-i", type=int, default=50, help="How many generations to stop after no improvement. Defaults to 50.")
 @click.option("-p", type=int, default=100, help="Population size. Defaults to 100.")
@@ -94,9 +106,9 @@ def aa(filepath, mode, trans_table, length, stop_codon, output, verbose):
 @click.option("-c", type=float, default=0.8, help="Crossover rate. Defaults to 0.8.")
 @click.option("-t", "--trans-table", type=int, default=11, help="The translation table to use. Defaults to 11, the standard genetic code.")
 @click.option("-o", '--output', type=click.Path(exists=False, dir_okay=False))
-def generate(aa_seq, freqs, verbose, i, p, m, c, trans_table, output):
+def generate(seq, freqs, verbose, i, p, m, c, trans_table, output):
     optimized = _generate(yaml.load(open(freqs)),
-                          SeqIO.read(aa_seq, "fasta").seq,
+                          str(SeqIO.read(seq, "fasta").seq),
                           verbose=verbose,
                           max_gens_since_improvement=i,
                           population_size=p,
