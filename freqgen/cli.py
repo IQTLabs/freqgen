105b98a6228e5e41d5720d24489c84b3e77cb9a9
Benjamin Lee <benjamindlee@me.com>
2018-09-30 20:39:23 -0400

    Remove redundant code

--- freqgen/cli.py
+++ freqgen/cli.py
@@ -29,14 +29,7 @@ def featurize(filepath, k, codon_usage, output):
             seq = str(seq.seq)
             seqs.append(seq)
 
-    result = k_mer_frequencies(seqs, k, include_missing=True)
-
-    # get the codon usage frequencies
-    if codon_usage:
-        for seq in seqs:
-            if len(seq) % 3 != 0:
-                raise ValueError("Cannot calculate codons for sequence whose length is not divisible by 3")
-        result["codons"] = codon_frequencies("".join(seqs))
+    result = k_mer_frequencies(seqs, k, include_missing=True, codons=codon_usage)
 
     if output:
         yaml.dump(result, open(output, "w+"), default_flow_style=False)
