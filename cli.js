2d89eb316d716d0f0eae2e56ee833128a20a9ab4
Benjamin Lee <benjamindlee@me.com>
2019-05-24 00:19:01 -0700

    Clean up the featurization CLI

--- cli.js
+++ cli.js
@@ -18,22 +18,25 @@ program
   .option('-c, --codons', 'Whether to featurize codons')
   .option('-o, --output <file>', 'The output YAML file')
   .action(function (files, options) {
-    const spinner = ora(`Parsing ${files.length} files...`).start()
+    // start up a pretty spinner (and correctly use file vs files!)
+    const spinner = ora(`Parsing ${files.length} file${files.length > 1 ? 's' : ''}...`).start()
 
     if (options.kMers == null) {
       spinner.fail('No k-mers or codons specified to featurize. Provide at least one k value after -k or use the -c flag to featurize codons.')
       return
     }
 
+    // parse the FASTA files into a flat list. Ex: ["ATGC...", "GTCAA...", ...]
     let seqs = files.map(file => Fasta.parse(fs.readFileSync(file, 'utf8')).map(obj => obj.seq))
     seqs = _.flatten(seqs)
 
-    let totalKmerCounts = {}
-
+    let totalKmerCounts = {} // will map k values to objs with k-mers and counts. Ex: {1: {"A": n, "T": n}...}
     for (let k of options.kMers) {
       spinner.text = `Counting ${k}-mers...`
       for (let seq of seqs) {
         let counts = freqgen.kmerCounts(freqgen.kmers(seq, k))
+
+        // add the new counts to the existing k-mer counts
         totalKmerCounts[k] = _.mergeWith({}, totalKmerCounts[k],
           counts, function (objValue, srcValue) {
             return _.isNumber(objValue) ? objValue + srcValue : srcValue
@@ -41,10 +44,13 @@ program
       }
     }
 
+    // if codon featurization is requested, count the codons of every seq
     if (!(options.codons == null)) {
       spinner.text = `Counting codons...`
       for (let seq of seqs) {
         let counts = freqgen.kmerCounts(freqgen.kmers(seq, 3, overlap = false))
+
+        // add the new counts to the existing codon counts
         totalKmerCounts['codons'] = _.mergeWith({}, totalKmerCounts['codons'],
           counts, function (objValue, srcValue) {
             return _.isNumber(objValue) ? objValue + srcValue : srcValue
@@ -54,9 +60,9 @@ program
 
     let kmerFrequencies = _.mapValues(totalKmerCounts, kmerCounts => freqgen.kmerFrequencies(kmerCounts))
 
-    // console.log(files, options.kMers || 0, !(options.codons == null))
+    spinner.succeed(`Done featurizing ${files.length} file${files.length > 1 ? 's' : ''}! ${options.output == null ? '' : 'Output written to ' + options.output + '.'}`)
 
-    spinner.succeed('Done!')
+    // either write to a file or print it out
     if (options.output == null) {
       console.log(yaml.safeDump(kmerFrequencies))
     } else {
