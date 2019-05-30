0416eba1702b169f46f8920c2e918bce1a36703c
Benjamin Lee <benjamindlee@me.com>
2019-05-30 01:35:07 -0700

    Make jest run eslint when watching!

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ['packages/**/*.js', '!**/node_modules/**'],
-  roots: ['packages/'],
+  projects: [
+    {
+      displayName: 'test',
+      roots: ['packages/'],
+      collectCoverage: true,
+      collectCoverageFrom: ['packages/**/*.js', '!**/node_modules/**'],
+    },
+    {
+      displayName: 'lint',
+      runner: 'jest-runner-eslint',
+      roots: ['packages/'],
+    },
+  ],
 }
